ef7862d027983096fecf5fc0f8e064d3a075fd69
Ivan Ralchev <iraltchev@gmail.com>
2018-02-18 16:26:06 +0200

    Removed some unnecessary lines from the file :)

--- tempy_db.py
+++ tempy_db.py
@@ -6,10 +6,6 @@ Usage:
     in provided SQLite database.
 """
 import sqlite3 as sqlite
-
-# TIME
-# INTTEMP
-# EXTTEMP
     
 database = sqlite.connect("tempy.db")
 
@@ -21,10 +17,3 @@ def data_write(time, int_temp, ext_temp):
                        )
         database.commit()
         cursor.close()
-
-"""
-This is the external temp data
-"""
-#external_temp = weather_data()
-#current_external_temp = external_temp["temp"]
-#print(current_external_temp)
