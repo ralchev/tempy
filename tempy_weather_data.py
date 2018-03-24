6d94c45613d5c71db5d76ab118854885a0ff2d05
Ivan Ralchev <iraltchev@gmail.com>
2018-03-24 12:45:49 +0200

    Revised API integration to handle errors and log them

--- tempy_weather_data.py
+++ tempy_weather_data.py
@@ -17,14 +17,13 @@ def weather_data():
         "q": "Sofia,bg",
         "units": "metric",
         "lang": "bg",
-        "appid": "33528e2b76c5e8219b4aa0f64194cb0f"
+        "appid": ""
         }
 
     try:
         data_current = requests.get(current, params=params).json()
         external_temp = data_current["main"]["temp"]
-        logging.info("SUCCESS")
         return({"temp": external_temp})
     except Exception as e:
-        logging.exception("EROOR, MATE!")
+        logging.exception("ERROR")
         weather_data()
