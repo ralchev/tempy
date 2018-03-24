9368748edb55b9625a091d9e539fe317580222f7
Ivan Ralchev <iraltchev@gmail.com>
2018-03-24 10:42:25 +0200

    Revised API integration to handle errors and log them

--- tempy_weather_data.py
+++ tempy_weather_data.py
@@ -1,27 +1,30 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Usage:
-    A simple request through the OpenWeatherMap API to call for current
-    temperatue in a particular city.
-    Returns the temperature.
+Created on Sun Feb 18 10:48:26 2018
+
+@author: ralchev
 """
 
 import requests
 import sys
+import logging
 
 def weather_data():
+    logging.basicConfig(filename="error.log", level=logging.INFO)
     current = "http://api.openweathermap.org/data/2.5/weather"
     params = {
-        "q": "", # City and Country code go here
+        "q": "Sofia,bg",
         "units": "metric",
-        "lang": "", # Your language preferences
-        "appid": "" #APIKEY goes here
+        "lang": "bg",
+        "appid": "33528e2b76c5e8219b4aa0f64194cb0f"
         }
 
     try:
         data_current = requests.get(current, params=params).json()
         external_temp = data_current["main"]["temp"]
+        logging.info("SUCCESS")
         return({"temp": external_temp})
-    except:
-        sys.exit(0)
+    except Exception as e:
+        logging.exception("EROOR, MATE!")
+        weather_data()
